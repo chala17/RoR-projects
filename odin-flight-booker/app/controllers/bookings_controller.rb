207d496a5ef31402e0b29fb7ecb6ad32fc0fac2a
chala17 <donathin17@gmail.com>
2021-02-03 15:05:14 -0800

    finished backend logic

--- odin-flight-booker/app/controllers/bookings_controller.rb
+++ odin-flight-booker/app/controllers/bookings_controller.rb
@@ -10,4 +10,24 @@ class BookingsController < ApplicationController
     end
   end
 
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking.id), notice: "Your booking has been saved! Safe travels!"
+    else
+      @flight = Flight.find(booking_params[:flight_id])
+      flash.now[:alert] = 'No fields can be left blank!'
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+  end
 end
